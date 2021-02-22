5a6ebad8e1a025bc589730d5a9ad82561fa2a315
Robbie Meyer <robbie.meyer12@gmail.com>
2021-02-22 16:47:15 -0500

    Update model with trained from fulltext

--- custom_transformers/keyword_transformers.py
+++ custom_transformers/keyword_transformers.py
@@ -1,15 +1,16 @@
 import re
 
 from sklearn.base import BaseEstimator, TransformerMixin
+from nltk import WordNetLemmatizer
 
 class CleanerTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, stop_words):
         self._stop_words = stop_words
         pass
-    
+
     def fit(self):
         pass
-    
+
     def transform(self, hl):
         # Remove punctuation
         hl = re.sub('[^a-zA-Z]', ' ', str(hl))
@@ -25,30 +26,30 @@ class CleanerTransformer(BaseEstimator, TransformerMixin):
         hl = re.sub("(\\d|\\W)+"," ", hl)
 
         # Convert to list from string
-        #hl = hl.split()
+        hl = hl.split()
 
         # Stemming (also does lowercase and removes punctuation)
-        #ps = PorterStemmer()
-        #hl = [ps.stem(x) for x in hl]
+        # ps = PorterStemmer()
+        # hl = [ps.stem(x) for x in hl]
 
         # Lemmatisation
-        #lem = WordNetLemmatizer()
-        #hl = [lem.lemmatize(word) for word in hl 
-        #      if not word in self._stop_words] 
-        return [hl]
-    
+        lem = WordNetLemmatizer()
+        hl = [lem.lemmatize(word) for word in hl
+              if not word in self._stop_words]
+        return [' '.join(hl)]
+
 class ScoreTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, cv):
         self._cv = cv
         pass
-    
+
     def fit(self):
         pass
-    
+
     def predict(self, X, n=5):
         sorted_items = self._sort_coo(X.tocoo())
         return self._extract_topn_from_vector(self._cv.get_feature_names(), sorted_items, n)
-    
+
     def _sort_coo(self, coo_matrix):
         tuples = zip(coo_matrix.col, coo_matrix.data)
         return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
@@ -59,13 +60,7 @@ class ScoreTransformer(BaseEstimator, TransformerMixin):
         #use only topn items from vector
         sorted_items = sorted_items[:topn]
 
-        score_vals = []
-        feature_vals = []
-
-        # word index and corresponding tf-idf score
-        for idx, score in sorted_items:
-            #keep track of feature name and its corresponding score
-            score_vals.append(round(score, 3))
-            feature_vals.append(feature_names[idx])
+        feature_vals = [feature_names[idx] for idx, _ in sorted_items]
+        score_vals = [round(score, 3) for _, score in sorted_items]
 
         return feature_vals, score_vals
